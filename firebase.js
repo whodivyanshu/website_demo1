7b8934dae1975e5b10a293baf2c916bd894aa2fb
DivyanshuLohar <divyanshulohar2002@gmail.com>
2023-05-31 11:01:23 +0530

    commit

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBgXEb8b09AwI_PkP2GDKUaEdSO_NxOUD4",
+  authDomain: "enhanced-kiln-318415.firebaseapp.com",
+  projectId: "enhanced-kiln-318415",
+  storageBucket: "enhanced-kiln-318415.appspot.com",
+  messagingSenderId: "1010045976229",
+  appId: "1:1010045976229:web:ca9970726f43f17934574c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
